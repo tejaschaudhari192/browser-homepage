d34fb6ce7769836bef0b33defbb69f52fb7ec8b0
Tejas Chaudhari <104405128+tejaschaudhari192@users.noreply.github.com>
2023-11-21 14:49:13 +0530

    reverdted

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function updateClock() {
     const now = new Date();
     const day = now.getDay().toString();
+    const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     // const seconds = now.getSeconds().toString().padStart(2, '0');
 
-    let hours = now.getHours();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12 || 12;
-    const hours = now.getHours().toString().padStart(2, '0');
-
-
-    const timeString = `${hours}:${minutes} ${ampm}`;
+    const timeString = `${hours}:${minutes}`;
     const dayString = `${day}`;
     document.getElementById('clock').textContent = timeString;
 
